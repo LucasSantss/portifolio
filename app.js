b6958ec53ebd65c13ba1c3da136c7868427f9764
Lucas Rodrigues <lucas.santosrod@hotmail.com>
2025-01-23 19:26:07 -0300

    efeitos no scroll

--- app.js
+++ app.js
@@ -30,6 +30,20 @@ function handleScroll() {
         }
     });
 }
+
+
 sidebar.addEventListener('scroll', handleScroll);
 
 
+const myObserver = new IntersectionObserver((entries) =>{
+    entries.forEach((entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    }))
+    console.log(entries)
+})
+const elements = document.querySelectorAll('.hidden')
+elements.forEach((element) => myObserver.observe(element))
