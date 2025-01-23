6f7556a68786d42243a631eb4becb90bfe6eb0f9
Lucas Rodrigues <lucas.santosrod@hotmail.com>
2025-01-23 17:14:20 -0300

    responsividade

---
+++ app.js
@@ -0,0 +1,35 @@
+const projects = document.querySelectorAll('.projects');
+const sidebar = document.querySelector('.projects');
+const cards = document.querySelectorAll('.card');
+const nxtBtn = document.querySelectorAll('.nxt-btn');
+const preBtn = document.querySelectorAll('.pre-btn');
+
+projects.forEach((item, i) =>{
+    let containerDimencao = item.getBoundingClientRect();
+    let containerWidth = containerDimencao.width;
+
+    nxtBtn[i].addEventListener('click',() => {
+        item.scrollLeft += containerWidth;
+    }) 
+
+    preBtn[i].addEventListener('click',() => {
+        item.scrollLeft -= containerWidth;
+    }) 
+})
+
+
+function handleScroll() {
+    const sidebarRect = sidebar.getBoundingClientRect();
+
+    cards.forEach(card => {
+    const cardRect = card.getBoundingClientRect();
+         if (cardRect.left < sidebarRect.left + 0 || cardRect.right > sidebarRect.right - 0) {
+          card.classList.add('fade-out');
+        } else {
+          card.classList.remove('fade-out');
+        }
+    });
+}
+sidebar.addEventListener('scroll', handleScroll);
+
+
